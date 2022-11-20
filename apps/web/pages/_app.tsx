34b535cbc05143ad3993b960bcfefffc5fddfe2b
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2022-11-20 01:14:44 +0100

    feat: supabase auth (#2)
    * feat: add supabase
    
    * feat: add supabase authentication

---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
+};
+
+export default MyApp;
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,5 +1,28 @@
-const Web = () => {
+import { GetServerSideProps } from "next";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+
+const Home = () => {
   return <h1>Web</h1>;
 };
 
-export default Web;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const supabase = createServerSupabaseClient(context);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session)
+    return {
+      redirect: {
+        destination: "/sign-in",
+        permanent: false,
+      },
+    };
+
+  return {
+    props: {},
+  };
+};
+
+export default Home;
